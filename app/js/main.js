9df1f84043f5f47945648ee6329b4601cec357be
Troy Madsen <troylmadsen@gmail.com>
2018-04-07 17:17:07 -0400

    - Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three';
+// import orbit from 'three-orbit-controls';
+// const OrbitControls = orbit(THREE);
+import TrackballControls from 'three-trackballcontrols';
+import Ghost from './models/Ghost';
+
+export default class App {
+  constructor() {
+    const c = document.getElementById('mycanvas');
+    // Enable antialias for smoother lines
+    this.renderer = new THREE.WebGLRenderer({canvas: c, antialias: true});
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, 4/3, 0.5, 500);
+    this.camera.position.z = 100;
+
+    // const orbiter = new OrbitControls(this.camera);
+    // orbiter.enableZoom = false;
+    // orbiter.update();
+    this.tracker = new TrackballControls(this.camera);
+    this.tracker.rotateSpeed = 2.0;
+    this.tracker.noZoom = false;
+    this.tracker.noPan = false;
+
+    this.ghost = new Ghost(0xd03e19);
+    this.ghost.matrixAutoUpdate = false;
+    this.scene.add(this.ghost);
+
+    const lightOne = new THREE.DirectionalLight(0xffffff, 1.0);
+    lightOne.position.set(10, 40, 100);
+    this.scene.add(lightOne);
+
+    const ambientLight = new THREE.AmbientLight(0x404040);
+    this.scene.add(ambientLight);
+
+    this.rotY1 = new THREE.Matrix4().makeRotationY(THREE.Math.degToRad(1));
+
+    window.addEventListener('resize', () => this.resizeHandler());
+    this.resizeHandler();
+    requestAnimationFrame(() => this.render());
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera);
+    this.tracker.update();
+
+    this.ghost.matrix.multiply (this.rotY1);
+
+    requestAnimationFrame(() => this.render());
+  }
+
+  resizeHandler() {
+    const canvas = document.getElementById("mycanvas");
+    let w = window.innerWidth - 16;
+    let h = 0.75 * w;  /* maintain 4:3 ratio */
+    if (canvas.offsetTop + h > window.innerHeight) {
+      h = window.innerHeight - canvas.offsetTop - 16;
+      w = 4/3 * h;
+    }
+    canvas.width = w;
+    canvas.height = h;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(w, h);
+    this.tracker.handleResize();
+  }
+}
