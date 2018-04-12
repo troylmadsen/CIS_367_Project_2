b041dbcb8ba0af5e22c15952d226152da102983f
Troy Madsen <troylmadsen@gmail.com>
2018-04-11 20:09:56 -0400

    - Began working on wave motion of boat

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,9 @@ import Ghost from './models/Ghost';
 import Lighthouse from './models/Lighthouse';
 import Mailbox from './models/Mailbox';
 
+//FIXME remove this
+import {BoxGeometry, Matrix4, Mesh, MeshPhongMaterial} from 'three';
+
 export default class App {
   constructor() {
     const c = document.getElementById('mycanvas');
@@ -23,6 +26,17 @@ export default class App {
     this.tracker.noZoom = false;
     this.tracker.noPan = false;
 
+    //FIXME replace this with boat
+    this.boat_deg = 0;
+    this.deg_change_rate = 2 * Math.PI / 2000;
+
+    var boatGeom = new BoxGeometry(5, 5, 10);
+    var boatMatr = new MeshPhongMaterial({color: 0x00ff00});
+    this.boat = new Mesh(boatGeom, boatMatr);
+    this.boat.matrixAutoUpdate = false;
+    var offset = new Matrix4().makeTranslation(0, 0, 20);
+    this.boat.matrix.multiply(offset);
+    this.scene.add(this.boat);
     // this.ghost = new Ghost(0x505050);
     // this.ghost.matrixAutoUpdate = false;
     // this.scene.add(this.ghost);
@@ -37,20 +51,33 @@ export default class App {
     const ambientLight = new THREE.AmbientLight(0x404040);
     this.scene.add(ambientLight);
 
-    this.rotY1 = new THREE.Matrix4().makeRotationY(THREE.Math.degToRad(1));
+    // this.rotY1 = new Matrix4().makeRotationY(THREE.Math.degToRad(1));
 
     window.addEventListener('resize', () => this.resizeHandler());
     this.resizeHandler();
-    requestAnimationFrame(() => this.render());
+    requestAnimationFrame((time) => this.render(time));
   }
 
-  render() {
+  render(ts) {
     this.renderer.render(this.scene, this.camera);
     this.tracker.update();
 
-    // this.ghost.matrix.multiply (this.rotY1);
+    // FIXME Boat angle changing
+    // this.boat_deg += this.deg_change_rate;
+    // if (this.boat_deg > 2 * Math.PI) {
+    //     this.boat_deg = 0;
+    // }
 
-    requestAnimationFrame(() => this.render());
+    // var bot_rot = new Matrix4().makeRotationX(Math.cos());
+    // this.boat.matrix.multiply(bot_rot);
+
+    var move = new Matrix4().makeTranslation(0, Math.sin(ts/500) / 4, 0);
+    var rot = new Matrix4().makeRotationX(Math.cos(ts/500));
+    console.log(THREE.Math.radToDeg(Math.cos(ts/500)));
+    // this.boat.matrix.multiply(move);
+    this.boat.matrix.multiply(rot);
+
+    requestAnimationFrame((time) => this.render(time));
   }
 
   resizeHandler() {
